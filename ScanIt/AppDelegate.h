2bc51f91a3621cd5d2a9c79fcb4ff76e4319b04d
NeilDeveloper <neil@nspiresoftware.net>
2016-03-11 15:28:05 -0500

    Initial Commit

---
+++ ScanIt/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ScanIt
+//
+//  Created by Yongyang Nie on 3/11/16.
+//  Copyright © 2016 Yongyang Nie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ScanIt/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ScanIt
+//
+//  Created by Yongyang Nie on 3/11/16.
+//  Copyright © 2016 Yongyang Nie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
